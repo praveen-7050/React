7b638ea2b5bb02fb81c5ba55e4f9a0339c1cf856
praveen-7050 <praveen.n7050@gmail.com>
2025-11-01 12:41:39 +0530

    adding react floder 2

---
+++ react-vite-2/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { Fragment } from "react";
+let name = "praveen";
+const gender = "Male";
+let islogged = true;
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <h1 style={{ backgroundColor: "red", color: "black" }}>React js vite</h1>
+        {islogged && (
+          <p className="text-warning
+           h1">
+            <i className="bi bi-emoji-smile-fill fs-3"></i> Welcome to React js vite
+          </p>
+        )}
+        <p>
+          Hello my Name is {name} and my gender is {gender}
+        </p>
+        <label htmlFor="age">Your age : </label>
+        <input id="age" type="number" />
+      </div>
+    </Fragment>
+  );
+}
+export default App;
